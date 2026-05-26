7a59c2cbede748b1584fa67e193281e98eb8e2d2
TitoPutraaa <titobamulo13@gmail.com>
2026-05-26 16:41:55 +0800

    navitation bar done

--- lib/core/ui/main_sheet.dart
+++ lib/core/ui/main_sheet.dart
@@ -1,4 +1,43 @@
+// import 'package:flutter/material.dart';
+
+// class MainSheet extends StatefulWidget {
+//   const MainSheet({super.key});
+
+//   @override
+//   State<MainSheet> createState() => _MainSheetState();
+// }
+
+// class _MainSheetState extends State<MainSheet> {
+//   int pageIndex = 1;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Second Brain"), centerTitle: true),
+//       bottomNavigationBar: BottomNavigationBar(
+//         onTap: (value) {
+//           setState(() {});
+//         },
+//         currentIndex: pageIndex,
+
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.sticky_note_2),
+//             label: "Note",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.favorite),
+//             label: "Favorite",
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:second_brain/features/notes/presentation/pages/favorite_page.dart';
+import 'package:second_brain/features/notes/presentation/pages/home_page.dart';
 
 class MainSheet extends StatefulWidget {
   const MainSheet({super.key});
@@ -8,21 +47,22 @@ class MainSheet extends StatefulWidget {
 }
 
 class _MainSheetState extends State<MainSheet> {
-  int pageIndex = 0;
+  int _currentIndex = 0;
+
+  final List<Widget> screen = [HomePage(), FavoritePage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Second Brain"), centerTitle: true),
+      body: IndexedStack(index: _currentIndex, children: screen),
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) {
-          setState(() {});
-        },
-        currentIndex: pageIndex,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.sticky_note_2),
-            label: "Note",
+            label: "Home",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
--- lib/features/notes/domain/repositories/note_repo.dart
+++ lib/features/notes/domain/repositories/note_repo.dart
@@ -1,4 +1,3 @@
-import 'package:second_brain/features/notes/domain/entities/kategori.dart';
 import 'package:second_brain/features/notes/domain/entities/note.dart';
 
 abstract class NoteRepo {
